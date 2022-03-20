35de7efad897c23614acbfc519282e594866e62e
1onedd1 <to4kanazna4eniya@mail.ru>
2022-03-20 18:11:48 +0200

    implemented factorial

---
+++ algorithms/factorial.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int factorial(int n)
+{
+    if(n == 0) return 1;
+
+    int result = 1;
+
+    for(int i = 1; i <= n; i++) 
+    {
+        result *= i;
+    }
+
+    return result;
+}
