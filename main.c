65914dbcbb4a8705d6e2f6b3eb5dc872ecbe63d0
1onedd1 <to4kanazna4eniya@mail.ru>
2022-03-20 17:28:09 +0200

    changed array filling with random numbers

--- main.c
+++ main.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 #include "main.h"
+#include <stdlib.h>
 
 void print_array(int* array, int length);
 
 int main(int argc, const char * argv[]) 
 {
-	int int_array[] = {2, 5, 1, 5, 6, 1};
+	int int_array[40] = {0};
 
 	int size = sizeof(int_array) / sizeof(int_array[0]);
+	for(int i = 0; i < size; i++) {
+		*(int_array + i) = rand() % 100; 
+	}
 
 	bubble_sort(int_array, size);
 
